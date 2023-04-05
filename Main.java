542f6c38ee4e95851769f3fc1e26ec059c77ee5a
LADISMTZ <122698443+LADISMTZ@users.noreply.github.com>
2023-04-04 20:39:45 -0600

    TERMINE

--- Main.java
+++ Main.java
@@ -96,8 +96,57 @@ public class Main {
        mexicosDistances.addEdgeByName("GD","GT",292.0);
        mexicosDistances.addEdgeByName("GD","ZS",321.0);
 
+       mexicosDistances.addEdgeByName("GT","QR",129.0);
+       mexicosDistances.addEdgeByName("GT","SP",207.0);
+       mexicosDistances.addEdgeByName("GT","ZS",357.0);
+       mexicosDistances.addEdgeByName("GT","MA",177.0);
 
+       mexicosDistances.addEdgeByName("HS","ML",695.0);
 
+       mexicosDistances.addEdgeByName("LC","MA",314.0);
+
+       mexicosDistances.addEdgeByName("LP","ML",1350.0);
+       mexicosDistances.addEdgeByName("LP","ES",1402.0);
+
+       mexicosDistances.addEdgeByName("MA","TC",236.0);
+
+       mexicosDistances.addEdgeByName("MX","QR",250.0);
+       mexicosDistances.addEdgeByName("MX","PB",135.0);
+       mexicosDistances.addEdgeByName("MX","PC",116.0);
+       mexicosDistances.addEdgeByName("MX","TC",63.0);
+       mexicosDistances.addEdgeByName("MX","TX",137.0);
+
+       mexicosDistances.addEdgeByName("MY","ST",87.0);
+       mexicosDistances.addEdgeByName("MY","SP",565.0);
+
+       mexicosDistances.addEdgeByName("OX","PB",340.0);
+       mexicosDistances.addEdgeByName("OX","TG",545.0);
+       mexicosDistances.addEdgeByName("OX","VZ",372.0);
+
+       mexicosDistances.addEdgeByName("PB","PC",389.0);
+       mexicosDistances.addEdgeByName("PB","TX",39.0);
+       mexicosDistances.addEdgeByName("PB","XP",190.0);
+       mexicosDistances.addEdgeByName("PB","VZ",274.0);
+
+       mexicosDistances.addEdgeByName("PC","QR",228.0);
+       mexicosDistances.addEdgeByName("PC","TX",129.0);
+       mexicosDistances.addEdgeByName("PC","XP",618.0);
+       mexicosDistances.addEdgeByName("PC","SP",618.0);
+
+       mexicosDistances.addEdgeByName("QR","SP",465.0);
+       mexicosDistances.addEdgeByName("QR","TC",196.0);
+
+       mexicosDistances.addEdgeByName("SP","ZS",194.0);
+       mexicosDistances.addEdgeByName("SP","XP",911.0);
+
+       mexicosDistances.addEdgeByName("ST","ZS",385.0);
+
+       mexicosDistances.addEdgeByName("TG","VH",246.0);
+       mexicosDistances.addEdgeByName("TG","VZ",553.0);
+
+       mexicosDistances.addEdgeByName("VH","VZ",467.0);
+
+       mexicosDistances.addEdgeByName("VZ","XP",107.0);
 
 
        mexicosDistances.showGraph();
@@ -147,6 +196,110 @@ public class Main {
        mexicosTimes.addNodeName("XP");
        mexicosTimes.addNodeName("ZS");
 
+       mexicosTimes.addEdgeByName("AC","ZS",85.0);
+       mexicosTimes.addEdgeByName("AC","GD",153.0);
+
+       mexicosTimes.addEdgeByName("AT","CA",141.0);
+       mexicosTimes.addEdgeByName("AT","XP",351.0);
+       mexicosTimes.addEdgeByName("AT","SP",385.0);
+
+       mexicosTimes.addEdgeByName("CA","MY",213.0);
+       mexicosTimes.addEdgeByName("CA","SP",239.0);
+
+       mexicosTimes.addEdgeByName("CH","HS",720.0);
+       mexicosTimes.addEdgeByName("CH","DG",437.0);
+       mexicosTimes.addEdgeByName("CH","CN",872.0);
+
+       mexicosTimes.addEdgeByName("CL","MD",301.0);
+       mexicosTimes.addEdgeByName("CL","CP",353.0);
+
+       mexicosTimes.addEdgeByName("CM","MZ",80.0);
+       mexicosTimes.addEdgeByName("CM","GD",143.0);
+       mexicosTimes.addEdgeByName("CM","MA",460.0);
+
+       mexicosTimes.addEdgeByName("CN","TP",318.0);
+       mexicosTimes.addEdgeByName("CN","DG",342.0);
+       mexicosTimes.addEdgeByName("CN","HS",487.0);
+       mexicosTimes.addEdgeByName("CN","CH",872.0);
+
+       mexicosTimes.addEdgeByName("CO","CV",122.0);
+       mexicosTimes.addEdgeByName("CO","PB",529.0);
+       mexicosTimes.addEdgeByName("CO","OX",817.0);
+       mexicosTimes.addEdgeByName("CO","TC",281.0);
+       mexicosTimes.addEdgeByName("CO","MA",589.0);
+       mexicosTimes.addEdgeByName("CO","LC",370.0);
+
+       mexicosTimes.addEdgeByName("CP","MD",139.0);
+       mexicosTimes.addEdgeByName("CP","VH",339.0);
+
+       mexicosTimes.addEdgeByName("CV","MX",80.0);
+       mexicosTimes.addEdgeByName("CV","PB",125.0);
+       mexicosTimes.addEdgeByName("CV","CO",122.0);
+       mexicosTimes.addEdgeByName("CV","TC",128.0);
+
+       mexicosTimes.addEdgeByName("DG","ZS",199.0);
+       mexicosTimes.addEdgeByName("DG","TP",638.0);
+       mexicosTimes.addEdgeByName("DG","ST",320.0);
+
+       mexicosTimes.addEdgeByName("ES","ML",191.0);
+       mexicosTimes.addEdgeByName("ES","LP",1009.0);
+
+       mexicosTimes.addEdgeByName("GD","TP",156.0);
+       mexicosTimes.addEdgeByName("GD","MA",184.0);
+       mexicosTimes.addEdgeByName("GD","GT",205.0);
+       mexicosTimes.addEdgeByName("GD","ZS",294.0);
+
+       mexicosTimes.addEdgeByName("GT","QR",106.0);
+       mexicosTimes.addEdgeByName("GT","SP",150.0);
+       mexicosTimes.addEdgeByName("GT","ZS",282.0);
+       mexicosTimes.addEdgeByName("GT","MA",138.0);
+
+       mexicosTimes.addEdgeByName("HS","ML",492.0);
+
+       mexicosTimes.addEdgeByName("LC","MA",234.0);
+
+       mexicosTimes.addEdgeByName("LP","ML",961.0);
+       mexicosTimes.addEdgeByName("LP","ES",1009.0);
+
+       mexicosTimes.addEdgeByName("MA","TC",171.0);
+
+       mexicosTimes.addEdgeByName("MX","QR",293.0);
+       mexicosTimes.addEdgeByName("MX","PB",127.0);
+       mexicosTimes.addEdgeByName("MX","PC",128.0);
+       mexicosTimes.addEdgeByName("MX","TC",73.0);
+       mexicosTimes.addEdgeByName("MX","TX",141.0);
+
+       mexicosTimes.addEdgeByName("MY","ST",70.0);
+       mexicosTimes.addEdgeByName("MY","SP",429.0);
+
+       mexicosTimes.addEdgeByName("OX","PB",258.0);
+       mexicosTimes.addEdgeByName("OX","TG",507.0);
+       mexicosTimes.addEdgeByName("OX","VZ",478.0);
+
+       mexicosTimes.addEdgeByName("PB","PC",469.0);
+       mexicosTimes.addEdgeByName("PB","TX",54.0);
+       mexicosTimes.addEdgeByName("PB","XP",174.0);
+       mexicosTimes.addEdgeByName("PB","VZ",210.0);
+
+       mexicosTimes.addEdgeByName("PC","QR",212.0);
+       mexicosTimes.addEdgeByName("PC","TX",122.0);
+       mexicosTimes.addEdgeByName("PC","XP",661.0);
+       mexicosTimes.addEdgeByName("PC","SP",649.0);
+
+       mexicosTimes.addEdgeByName("QR","SP",416.0);
+       mexicosTimes.addEdgeByName("QR","TC",156.0);
+
+       mexicosTimes.addEdgeByName("SP","ZS",131.0);
+       mexicosTimes.addEdgeByName("SP","XP",727.0);
+
+       mexicosTimes.addEdgeByName("ST","ZS",244.0);
+
+       mexicosTimes.addEdgeByName("TG","VH",222.0);
+       mexicosTimes.addEdgeByName("TG","VZ",430.0);
+
+       mexicosTimes.addEdgeByName("VH","VZ",356.0);
+
+       mexicosTimes.addEdgeByName("VZ","XP",89.0);
 
 
        mexicosTimes.showGraph();
